0c716baf1111d03eb0c648c53bf9b0946c4b0241
urzas-droid <wonderlandmelech@gmail.com>
2024-06-07 17:53:58 +0200

    documents entfernt

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,63 @@
 <template>
-  <div id="header">  
-        <h1>Weather Widget App</h1>
+  <div id="header">
+    <h1>Weather Widget App</h1>
   </div>
 
   <div id="widgetContainer">
-    <Widget id="baseWidget" @click="toggleForecast"></Widget>
-    <Widget1 id="baseWidget1" @click="toggleForecast"></Widget1>
+    <Widget
+      v-for="element in locations"
+      :location="element"
+      id="baseWidget"
+      @click="toggleForecast"
+    ></Widget>
   </div>
 
-  <div style="text-align: center;">
+  <div style="text-align: center">
     <button id="addLocation" @click="addLocation">+</button>
   </div>
 </template>
 
-
-
 <script setup lang="ts">
-  import Widget from './components/weather_widget.vue';
-  import Widget1 from './components/custom_weather_widget.vue';
+import { ref } from "vue";
+import Widget from "./components/weather_widget.vue";
 
+const locations = ref<string[]>(["Berlin"]);
 
-  function addLocation(){
-    const node = document.createElement("Widget");
-    const textnode = document.createTextNode("WATER");
-    node.appendChild(textnode);
-    document.getElementById("widgetContainer")?.appendChild(node);
+function addLocation() {
+  locations.value.push("London");
+}
 
-   }
-  
-  function toggleForecast(){
-    if (document.getElementById("unterbehaelter3")?.style.display === "none"){
-      document.getElementById("unterbehaelter3").style.display = "block";
-    } else {document.getElementById("unterbehaelter3").style.display = "none";}
+function toggleForecast() {
+  if (document.getElementById("unterbehaelter3")?.style.display === "none") {
+    document.getElementById("unterbehaelter3").style.display = "block";
+  } else {
+    document.getElementById("unterbehaelter3").style.display = "none";
   }
-  
+}
 </script>
 
-<style >
-  #addLocation{
-    font-size: 25px;
-    font-weight: bolder;
-    color: rgb(139, 136, 136, 0.5);
-    border-radius: 40px;
-    border-color: rgba(134, 134, 134, 0.5);
-    background: rgb(47,79,79,0.5);
-    align-items: center;
-    height: 50px;
-    width: 50px;
-  }
+<style>
+#addLocation {
+  font-size: 25px;
+  font-weight: bolder;
+  color: rgb(139, 136, 136, 0.5);
+  border-radius: 40px;
+  border-color: rgba(134, 134, 134, 0.5);
+  background: rgb(47, 79, 79, 0.5);
+  align-items: center;
+  height: 50px;
+  width: 50px;
+}
 
-  #addLocation:hover{
-    font-size: 25px;
-    font-weight: bolder;
-    color: rgb(136, 136, 136, 1);
-    border-radius: 40px;
-    border-color: white;
-    background: rgb(47,79,79,1);
-    align-items: center;
-    height: 50px;
-    width: 50px;
-  }
+#addLocation:hover {
+  font-size: 25px;
+  font-weight: bolder;
+  color: rgb(136, 136, 136, 1);
+  border-radius: 40px;
+  border-color: white;
+  background: rgb(47, 79, 79, 1);
+  align-items: center;
+  height: 50px;
+  width: 50px;
+}
 </style>
-
-
-
-
-
-
-
-
--- src/components/weather_widget.vue
+++ src/components/weather_widget.vue
@@ -1,166 +1,197 @@
 <template>
-    <div class="behaelter">        
-        <div id="ort"></div>
-        <div class="unterbehaelter">
-            <img id="icon"></img>
-            <div id="temperatur"></div>
-            <div id="minmax"></div>
-            </div>
-            <div class="unterbehaelter" id="unterbehaelter2">
-                <div id="beschreibung"></div>
-                <div id="datum"></div>
-            </div>
-            <div class="unterbehaelter collapse" id="unterbehaelter3" @click="">
-                <div class="forecast" id="forecast1"></div>
-                <div class="forecast" id="forecast2"></div>
-            </div>
-    </div>    
+  <div class="behaelter">
+    <div id="ort">{{ weatherData?.location.region }}</div>
+    <div class="unterbehaelter">
+      <img id="icon" :src="weatherData?.current.condition.icon" />
+      <div id="temperatur">{{ weatherData?.current.temp_c }}&deg;</div>
+      <div id="minmax">
+        <span
+          >Max: {{ weatherData?.forecast.forecastday[0].day.maxtemp_c }}&deg;
+        </span>
+        <span
+          >Min:
+          {{ weatherData?.forecast.forecastday[0].day.mintemp_c }}&deg;</span
+        >
+      </div>
+    </div>
+    <div class="unterbehaelter" id="unterbehaelter2">
+      <div id="beschreibung">{{ weatherData?.current.condition.text }}</div>
+      <div id="datum">
+        {{ days[d.getDay()] }}, {{ d.getDate() }}. {{ months[d.getMonth()] }}
+      </div>
+    </div>
+    <div class="unterbehaelter" id="unterbehaelter3" @click="">
+      <div class="forecast" id="forecast1">
+        {{ weatherData?.forecast.forecastday[1].day.maxtemp_c }}&deg;
+      </div>
+      <div class="forecast" id="forecast2">
+        {{ weatherData?.forecast.forecastday[2].day.maxtemp_c }}&deg;
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-   .behaelter{
-        width: fit-content;
-        padding: 20px;
-        margin: 100px auto;
-        background: rgba(37, 36, 37, 0.5);
-        border: 2px;
-        border-radius: 10px;
-   }
+<style scoped>
+.behaelter {
+  width: fit-content;
+  padding: 20px;
+  margin: 100px auto;
+  background: rgba(37, 36, 37, 0.5);
+  border: 2px;
+  border-radius: 10px;
+}
 
-   .unterbehaelter{
-        display:flex;
-   }
+.unterbehaelter {
+  display: flex;
+}
 
-   .forecast{
-        font-family: Arial, Helvetica, sans-serif;
-        text-transform: uppercase;
-        text-align: center;
-        font-weight: bold;
-        color: rgba(233, 233, 233, 0.8);
-        padding: 10px;
-        width: 40%;
-        margin: auto        
-   }
+.forecast {
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  text-align: center;
+  font-weight: bold;
+  color: rgba(233, 233, 233, 0.8);
+  padding: 10px;
+  width: 40%;
+  margin: auto;
+}
 
-   #unterbehaelter3{
-    display: none;
-   }
+#unterbehaelter3 {
+  display: none;
+}
 
-   #ort{
-        font-family: Arial, Helvetica, sans-serif;
-        text-transform: uppercase;
-        font-weight: bold;
-        text-align: center;
-        color: rgba(233, 233, 233, 0.8);
-   }
+#ort {
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  font-weight: bold;
+  text-align: center;
+  color: rgba(233, 233, 233, 0.8);
+}
 
-   #temperatur{
-        font-family: Arial, Helvetica, sans-serif;
-        text-transform: uppercase;
-        text-align: center;
-        vertical-align: text-bottom;
-        font-weight: bold;
-        font-size: 3em;
-        color: rgba(233, 233, 233, 0.8);
-        padding: 10px;
-        width: 30%;
-        margin: auto;        
-   }
+#temperatur {
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  text-align: center;
+  vertical-align: text-bottom;
+  font-weight: bold;
+  font-size: 3rem;
+  color: rgba(233, 233, 233, 0.8);
+  padding: 10px;
+  width: 30%;
+  margin: auto;
+}
 
-   #minmax{
-        font-family: Arial, Helvetica, sans-serif;
-        text-transform: uppercase;
-        text-align: left;
-        vertical-align: text-bottom;
-        font-weight: bold;
-        color: rgba(233, 233, 233, 0.8);
-        padding: 10px;
-        width: 30%;
-        margin: auto;        
-   }
+#minmax {
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  text-align: left;
+  vertical-align: text-bottom;
+  font-weight: bold;
+  color: rgba(233, 233, 233, 0.8);
+  padding: 10px;
+  width: 30%;
+  margin: auto;
+}
 
-    #datum{
-        font-family: Arial, Helvetica, sans-serif;
-        text-transform: uppercase;
-        text-align: left;
-        font-weight: bold;
-        color: rgba(233, 233, 233, 0.8);
-        padding: 10px;
-        margin-left: auto;        
-    }
+#datum {
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  text-align: left;
+  font-weight: bold;
+  color: rgba(233, 233, 233, 0.8);
+  padding: 10px;
+  margin-left: auto;
+}
 
-    #icon{
-        padding: 10px;
-        text-align: center;
-        margin: auto;        
-    }
-    
-    #beschreibung{
-        font-family: Arial, Helvetica, sans-serif;
-        text-transform: uppercase;
-        text-align: left;
-        font-weight: bold;
-        color: rgba(233, 233, 233, 0.8);
-        padding: 10px;
-    }
+#icon {
+  padding: 10px;
+  text-align: center;
+  margin: auto;
+}
 
-    #unterbehaelter2{        
-        border-bottom-style: solid;
-        border-color: rgba(0,0,0,0.5);
-    }
+#beschreibung {
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  text-align: left;
+  font-weight: bold;
+  color: rgba(233, 233, 233, 0.8);
+  padding: 10px;
+}
 
-   
+#unterbehaelter2 {
+  border-bottom-style: solid;
+  border-color: rgba(0, 0, 0, 0.5);
+}
 </style>
 
-<script>
-const d = new Date();
-const months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
-const days = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"];
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
 
-getWeatherJSON();
+const props = defineProps<{ location: string }>();
+
+type WeatherApiResponse = {
+  location: { region: string };
+  current: { temp_c: number; condition: { icon: string; text: string } };
+  forecast: {
+    forecastday: { day: { maxtemp_c: number; mintemp_c: number } }[];
+  };
+};
+const weatherData = ref<WeatherApiResponse>();
+
+onMounted(async () => {
+  weatherData.value = await getWeatherJSON();
+});
+
+const d = new Date();
+const months = [
+  "Januar",
+  "Februar",
+  "März",
+  "April",
+  "Mai",
+  "Juni",
+  "Juli",
+  "August",
+  "September",
+  "Oktober",
+  "November",
+  "Dezember",
+];
+const days = [
+  "Sonntag",
+  "Montag",
+  "Dienstag",
+  "Mittwoch",
+  "Donnerstag",
+  "Freitag",
+  "Samstag",
+];
+
+//getWeatherJSON();
 
 //////////// functions /////////
+//convert to vue
 
-
-
-function getPositionData(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(getWeatherJSON);
-    }
-    else{
-        console.log('Error: Geolocation not supported');
-    }
+function getDayOfWeek(x: number) {
+  if (d.getDay() + x > 6) {
+    x = d.getDay() + x - 7;
+  } else {
+    x = d.getDay() + x;
+  }
+  return x;
 }
 
-function getDayOfWeek(x){
-    if ((d.getDay() + x) > 6){
-        x = (d.getDay() + x) - 7;
-    } else {x = d.getDay() + x;}
-    return x;
+async function getWeatherJSON() {
+  const apiKey = "key=30189c89030a42629e195610240306%20";
+
+  const apiCall = `https://api.weatherapi.com/v1/forecast.json?q=${props.location}&days=3&aqi=no&alerts=no&`;
+
+  let call = apiCall + apiKey;
+
+  const response = await fetch(call);
+
+  const forecast: WeatherApiResponse = await response.json();
+  console.log(forecast);
+  return forecast;
 }
-
-async function getWeatherJSON(position){
-    const apiKey = 'key=30189c89030a42629e195610240306%20';
-    const apiCall = "https://api.weatherapi.com/v1/forecast.json?q=London&days=3&aqi=no&alerts=no&"
-        
-    let call = apiCall + apiKey;
-    
-    const response = await
-    fetch(call);
-    const forecast = await response.json();
-    console.log(forecast)
-
-    document.getElementById("ort").innerHTML = forecast.location.region;
-    document.getElementById("temperatur").innerHTML = forecast.current.temp_c + "&deg;";
-    document.getElementById("minmax").innerHTML = "Max: " + forecast.forecast.forecastday[0].day.maxtemp_c + "&deg;" + '<br>' + "Min: " + forecast.forecast.forecastday[0].day.mintemp_c + "&deg;";
-    document.getElementById("datum").innerHTML = days[d.getDay()-1] + ", " + d.getDate() + ". " + months[d.getMonth()];
-    document.getElementById("icon").src = forecast.current.condition.icon ;
-    document.getElementById("beschreibung").innerHTML = forecast.current.condition.text;
-    document.getElementById("forecast1").innerHTML = forecast.forecast.forecastday[1].day.maxtemp_c + "&deg" + '<br>' + days[getDayOfWeek(0)];
-    document.getElementById("forecast2").innerHTML = forecast.forecast.forecastday[2].day.maxtemp_c + "&deg" + '<br>' + days[getDayOfWeek(1)]; 
- 
-}
-
-
 </script>
-
